114a04d394de682e38fdacb992e502ce9113c7f8
Brent Martin <bmart35@gmail.com>
2016-05-11 17:53:15 -0500

    added update functionality in participation

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,6 +2,8 @@ class Participant < ActiveRecord::Base
   has_many :responses
   has_many :questions, through: :assessments
 
+  accepts_nested_attributes_for :responses
+
   belongs_to :user
   belongs_to :survey
 end
